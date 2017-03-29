b62d7dda600ba3119112075f9402f5788a461ea7
Nick Wormley <worm@hoffmanavenuesoftware.com>
2017-03-29 11:32:28 -0500

    Parse venue from tags as well

--- src/Mspiff/Scrape/Scrape.hs
+++ src/Mspiff/Scrape/Scrape.hs
@@ -5,30 +5,49 @@ module Mspiff.Scrape.Scrape
 import Mspiff.Model
 import qualified Data.Text.Lazy as T
 import qualified Data.List as DL
-import Data.ByteString.Lazy
+import Data.Aeson
+--import Data.ByteString.Lazy
 import Control.Monad
 import Data.Monoid
 import Data.Maybe
 import Data.Text.Lazy.Encoding
-import Network.URI.Encode
+import qualified Network.URI.Encode as E
 import Network.HTTP.Client
 import Text.HTML.TagSoup
+
 
 import Data.Time
 import Data.Time.Clock.POSIX
 
+type Name = T.Text
 data ScrapeScreening = ScrapeScreening
-  { screeningName :: T.Text
-  , screeningStartTime :: Int
+  { screeningName :: Name
+  , screeningStartTime :: Maybe UTCTime
   , screeningDuration :: Int
+  , screeningVenue :: Name
+  , screeningUrl :: T.Text
   }
   deriving (Show, Eq)
+
+parseDate :: T.Text -> Maybe UTCTime
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%z" . T.unpack
+
+instance FromJSON ScrapeScreening where
+  parseJSON (Object v) =
+    ScrapeScreening
+      <$> v .: "name"
+      <*> (parseDate <$> v .: "startDate")
+      <*> pure 0
+      <*> (v .: "location" >>= (.: "name"))
+      <*> (v .: "offers" >>= (.: "url"))
+  parseJSON _ = error "invalid screening json"
+
 
 scheduleUrlBase :: T.Text
 scheduleUrlBase = "http://prod3.agileticketing.net/WebSales/pages/list.aspx?epguid=87ecbce5-5fcd-406d-8bcb-88fa6bb54965&mdy="
 
 toDateUrl :: Day -> (Day, T.Text)
-toDateUrl d = (d, scheduleUrlBase <> T.pack (encode str) <> "&")
+toDateUrl d = (d, scheduleUrlBase <> T.pack (E.encode str) <> "&")
   where
     str = formatTime defaultTimeLocale "%-m/%d/%y" (UTCTime d 0)
 
@@ -39,17 +58,8 @@ processAll = join <$> mapM processUrl (toDateUrl <$> days)
 processUrl :: (Day, T.Text) -> IO [ScrapeScreening]
 processUrl (d, url) = processTags d . parseTags <$> hit url
 
-parseTOD :: Day -> T.Text -> Maybe Showtime
-parseTOD day t = (fromIntegral . floorInt . utcTimeToPOSIXSeconds) <$> toUtc
-   where
-     floorInt :: RealFrac a => a -> Int
-     floorInt = floor
-     t' = T.unpack (T.takeWhile (/='(') t)
-     toUtc = UTCTime day . utctDayTime <$> toTod
-     toTod = parseTimeM True defaultTimeLocale "%l:%M %p " t'
-
-processTags' :: [Tag T.Text] -> Maybe T.Text
-processTags' = go
+processLd :: [Tag T.Text] -> Maybe T.Text
+processLd = go
   where
     go [] = Nothing
     go (x:y:xs) | isLdJson x && isTagText y = Just (fromTagText y)
@@ -58,29 +68,29 @@ processTags' = go
       isTagOpenName "script" t && fromAttrib "type" t == "application/ld+json"
 
 processTags :: Day -> [Tag T.Text] -> [ScrapeScreening]
-processTags day = DL.reverse . go []
+processTags day tags = foldr combine [] (fromMaybe [] screenings)
   where
-    go :: [ScrapeScreening] -> [Tag T.Text] -> [ScrapeScreening]
-    go acc [] = acc
-    go acc list@(se:item:_:timeVal:close:name:nameVal:xs)
-      | isSE se && isName name && isItem item && isTagClose close =
-         go (toScreening se timeVal nameVal : acc) xs
-      | otherwise = go acc (DL.drop 1 list)
-    go acc _ = acc
+    screenings = processLd tags >>= decode' . encodeUtf8
 
-    isDiv = isTagOpenName "div"
+    durations = go [] tags
+      where
+        go :: [(Name,Int)] -> [Tag T.Text] -> [(Name,Int)]
+        go acc [] = acc
+        go acc list@(se:item:_:timeVal:close:name:nameVal:xs)
+          | isSE se && isName name && isItem item && isTagClose close =
+             go ( (fromTagText nameVal, parseDuration se) : acc) xs
+          | otherwise = go acc (DL.drop 1 list)
+        go acc _ = acc
+        parseDuration = read . T.unpack . fromAttrib "duration"
+        isDiv = isTagOpenName "div"
+        hasClass c t = isDiv t && fromAttrib "class" t == c
+        isSE = hasClass "ScheduledEvent"
+        isItem = hasClass "Item"
+        isName = hasClass "Name"
 
-    hasClass c t = isDiv t && fromAttrib "class" t == c
-    isSE = hasClass "ScheduledEvent"
-    isItem = hasClass "Item"
-    isName = hasClass "Name"
-    fromTime = fromMaybe 0 . parseTOD day
-
-    toScreening dur t name =
-      ScrapeScreening
-        (fromTagText name)
-        (fromTime (fromTagText t))
-        (read $ T.unpack (fromAttrib "duration" dur))
+    combine s@ScrapeScreening{..} acc =
+      let d = DL.lookup screeningName durations
+      in maybe (s:acc) (\d' -> s { screeningDuration = d' } : acc) d
 
 hit :: T.Text -> IO T.Text
 hit url = do

dbc83177a60a84fc08f37cfc7a76053d64e7dc82
Nick Wormley <worm@hoffmanavenuesoftware.com>
2017-03-30 10:25:54 -0500

    Skeletal main

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,45 @@
 
 module Main where
 
+import qualified Data.Text.Lazy.IO as T
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString as BS
+
+import qualified Data.Text as T
+import Data.FileEmbed
+
+-- import JavaScript.JQuery
+-- import JavaScript.JQuery.Internal
+
+import Lucid
+
 import Mspiff.Model
 import Mspiff.Loader
 import Mspiff.Scheduler
-import Test.QuickCheck
-import Test.QuickCheck.Arbitrary
-import qualified Data.List as DL
-import System.Environment
-import Data.Array
-import Control.Monad
-main :: IO ()
+import Mspiff.Html
+
+catalogJson :: BS.ByteString
+catalogJson = $(embedFile "data/catalog")
+
 main = do
-  (c:_) <- getArgs
-  let w = Schedule (elems screenings)
-  forever $ do
-    f <- replicateM (readInt c) (generate arbitrary) :: IO [Film]
-    case DL.take 1 (viewableSchedulesFor' w f) of
-      [x] -> print (showtimesForSchedule x)
-      _ -> print "NONE"
+  let
+    Just catalog = loadCatalog (LBS.fromStrict catalogJson)
+    html = renderText (renderWholeSchedule catalog)
+  T.putStrLn html
+
+{-
+
+  b <- select "body"
+  append "<div class='mouse'></div>" b
+  m <- select ".mouse"
+  mousemove (handler m) def b
+
+
+handler :: JQuery -> Event -> IO ()
+handler m e = do
+  x <- pageX e
+  y <- pageY e
+  setText (T.pack $ show (x,y)) m
+  return ()
+
+-}
